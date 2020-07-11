e6aa7e0f8c671ef1894ff318e982846389015efa
luckharder <68143263+luckharder@users.noreply.github.com>
2020-07-11 12:01:18 +0300

    upt net

--- approx_net.py
+++ approx_net.py
@@ -1,11 +1,14 @@
 import numpy as np
 import torch
 import pandas as pd
+import datetime
 
 
+# распиши пожалуста каждую колонку так по типам
+dtype_dict = {"date": datetime, "low_c":float}
 try:
-	train_set = pd.read_csv("datasets/train.csv", encoding="cp1251")
+	train_set = pd.read_csv("datasets/train.csv", encoding="cp1251", delimiter=';', dtype=dtype_dict)
 except FileNotFoundError:
 	pass
 
-x_train = torch.Tensor(train_set[])
+x_train = torch.Tensor(train_set[ "low_c"].values).float()
