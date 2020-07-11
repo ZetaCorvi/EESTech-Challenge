590b275bda3b8160a82dc1b69ffe797123a1cabb
luckharder <68143263+luckharder@users.noreply.github.com>
2020-07-11 11:47:32 +0300

    upt net

--- approx_net.py
+++ approx_net.py
@@ -1,1 +1,11 @@
-import csv
+import numpy as np
+import torch
+import pandas as pd
+
+
+try:
+	train_set = pd.read_csv("datasets/train.csv", encoding="cp1251")
+except FileNotFoundError:
+	pass
+
+x_train = torch.Tensor(train_set[])
